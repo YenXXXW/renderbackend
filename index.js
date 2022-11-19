0fed3a2f2bd54a26f191351b3de7132e286dc2e9
Yen <YenXXXW@gmail.com>
2022-11-19 22:26:11 +0630

    remove get(/)"

--- index.js
+++ index.js
@@ -25,9 +25,7 @@ app.use((req, res, next) => {
 app.use('/api/workouts', workoutRoutes)
 app.use('/api/user', userRoutes)
 
-app.get('/' ,(req, res)=>{
-  res.send('hello')
-})
+
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
