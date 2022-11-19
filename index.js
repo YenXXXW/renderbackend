97260d43035eefd4b841680b4b444ecd0fe8b516
Yen <YenXXXW@gmail.com>
2022-11-19 11:37:15 +0630

    vercel deploy cors added

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const app = express()
 const corsOptions ={
   origin: [
     'http://localhost:3000',
-    'https://renderfrontend.onrender.com'
+    'https://renderfrontend.onrender.com' ,
+    'https://renderfrontend.vercel.app/'
   ]
 }
 
