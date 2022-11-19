2822a3265ca5fe3f415e5ead27f1ca23f206d63b
Yen <YenXXXW@gmail.com>
2022-11-19 08:31:36 +0630

    the cors error check

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors({
-  origin : 'https://renderfrontend.onrender.com'
+  origin : "https://renderfrontend.onrender.com"
 }))
 
 // middleware
